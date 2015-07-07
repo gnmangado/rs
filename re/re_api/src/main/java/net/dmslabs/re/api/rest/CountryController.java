6d3ec0aedb24292a89df9bba76952e35542c00cb
Gonzalo Mangado <gnmangado@gmail.com>
2015-07-07 16:34:35 -0300

    Improving configuration files placement.

--- re/re_api/src/main/java/net/dmslabs/re/api/rest/CountryController.java
+++ re/re_api/src/main/java/net/dmslabs/re/api/rest/CountryController.java
@@ -4,10 +4,7 @@ package net.dmslabs.re.api.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/countries")
@@ -15,29 +12,5 @@ public class CountryController {
 
     static final Logger logger = LoggerFactory.getLogger(CountryController.class);
 
-    // @RequestMapping(value = "login", method = RequestMethod.POST, headers={"Accept=*/*", "Content-Type=application/json"})
-    // public @ResponseBody
-    // Map<String, String> login(@RequestBody final Globito employee) {
-    // logger.info("BEGIN::login");
-    // String token = null;
-    // Glober glober = GlobersManager.getInstance().getGloberByAuthentication(employee.getUsername(), employee.getPassword());
-    // if (glober != null) {
-    // token = SessionManager.getInstance().createSession(glober);
-    // } else {
-    // throw new UnauthorizedException();
-    // }
-    // Map<String, String> response = new HashMap<String, String>();
-    // response.put("session_token", token);
-    // return response;
-    // }
-
-//    @RequestMapping(value = "/{name}", method = RequestMethod.GET, headers = { "Accept=application/json" })
-//    public @ResponseBody
-//    Country testService(@PathVariable("name") String name) {
-//        logger.info("BEGIN::testService");
-//        Country c = new Country();
-//        c.setId(45);
-//        c.setName("Uruguay");
-//        return c;
-//    }
+    
 }
