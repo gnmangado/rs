82d53c1967b6bf377e10c461a7afb28683de1191
Gonzalo Mangado <gnmangado@gmail.com>
2015-07-12 01:21:48 -0300

    #7 Adding basic admin template, configured bower dependencies to include template dependencies.

--- re/re_web_backoffice/src/site/test/karma.conf.js
+++ re/re_web_backoffice/src/site/test/karma.conf.js
@@ -33,6 +33,10 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/raphael/raphael.js',
+      'bower_components/mocha/mocha.js',
+      'bower_components/morris-js/morris.js',
+      'bower_components/flot-charts/jquery.flot.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
